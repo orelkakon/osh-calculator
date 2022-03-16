6572acb3fa20d701813ef98379d0c5df32aa9a15
orelkakon <orelkak@post.bgu.ac.il>
2022-03-17 00:05:23 +0200

    start details page

--- src/screens/Details/index.tsx
+++ src/screens/Details/index.tsx
@@ -3,12 +3,24 @@ import { Text, View, SafeAreaView, Switch, TextInput } from 'react-native';
 import { styles } from "./style"
 
 export const Details = () => {
-
+    const [mass, setMass] = useState("")
+    const [height, setHeight] = useState("")
+    const [year, setYear] = useState("")
     return (
         <SafeAreaView style={styles.container}>
             <View>
-                <Text style={{ color: "white" }}>משקל</Text>
-                <TextInput style={styles.input} placeholder="משקל"></TextInput>
+                <View style={styles.row}>
+                    <Text style={styles.text}>משקל</Text>
+                    <TextInput keyboardType="numeric" style={styles.input} value={mass} placeholder={`ק"ג`} onChangeText={(num) => setMass(num)}></TextInput>
+                </View>
+                <View style={styles.row}>
+                    <Text style={styles.text}>גובה</Text>
+                    <TextInput keyboardType="numeric" style={styles.input} value={height} placeholder={`ס"מ`} onChangeText={(num) => setHeight(num)}></TextInput>
+                </View>
+                <View style={styles.row}>
+                    <Text style={styles.text}>גיל</Text>
+                    <TextInput keyboardType="numeric" style={styles.input} value={year} placeholder={`שנים`} onChangeText={(num) => setYear(num)}></TextInput>
+                </View>
             </View>
         </SafeAreaView>
     );
--- src/screens/Details/style.ts
+++ src/screens/Details/style.ts
@@ -4,14 +4,36 @@ export const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'flex-start'
+        alignItems: 'flex-start',
+        justifyContent: 'flex-start',
+        direction: "rtl"
+    },
+    row: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        borderBottomWidth: 2,
+        borderColor: 'gray',
+        width: '100%',
+        height: 60,
+        padding: 10
     },
     input: {
+        display: 'flex',
+        flexDirection: 'row',
+        alignSelf: 'center',
+        textAlign: 'center',
+        direction: "rtl",
         height: 40,
-        margin: 12,
+        width: 100,
+        borderRadius: 5,
         borderWidth: 1,
-        padding: 10,
-        color: "red"
+        backgroundColor: "white",
+        fontSize: 30,
+    },
+    text: {
+        color: "white",
+        fontSize: 40
     }
 })
